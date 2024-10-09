fecabe3f06a77973b85973c6a39ea369468b508a
Mylo-hz <kaunghtetzaw2514@gmail.com>
2024-10-09 15:36:58 -0700

    Profile 100% done

--- src/main/java/com/example/test/MainActivity.kt
+++ src/main/java/com/example/test/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -24,14 +25,28 @@ class MainActivity : AppCompatActivity() {
 //
 //            // this is a ttt
 //        }
+
+        val firstName = findViewById<EditText>(R.id.etFirstName)
+        val lastName = findViewById<EditText>(R.id.etLastName)
+        val streetAddress = findViewById<EditText>(R.id.etStreetAddress)
+        val city = findViewById<EditText>(R.id.etCity)
+
+        val province = findViewById<EditText>(R.id.etProvince)
+        val postalCode = findViewById<EditText>(R.id.etPostalCode)
+        val email = findViewById<EditText>(R.id.etEmailAddress)
+        val phoneNumber = findViewById<EditText>(R.id.etPhoneNumber)
+
         val btnSave = findViewById<Button>(R.id.btnSave)
         btnSave.setOnClickListener{
-            val firstName = findViewById<EditText>(R.id.etFirstName)
-            val lastName = findViewById<EditText>(R.id.etLastName)
-            val statusUpdate = findViewById<TextView>(R.id.tvStatusUpdate)
-            //tvStatusUpdate.text.to
+            firstName.setText(" ")
+            lastName.setText(" ")
+            streetAddress.setText(" ")
+            city.setText(" ")
+            province.setText(" ")
+            postalCode.setText(" ")
+            email.setText(" ")
+            phoneNumber.setText(" ")
+            Toast.makeText(this, "Changes have been saved!", Toast.LENGTH_SHORT).show()
         }
-
-        //}
     }
 }
