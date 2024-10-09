da44813f8fe7b677d1302c8b08f0c3f476034186
Mylo-hz <kaunghtetzaw2514@gmail.com>
2024-10-09 11:37:30 -0700

    Profile 90% done

--- src/main/java/com/example/test/MainActivity.kt
+++ src/main/java/com/example/test/MainActivity.kt
@@ -1,22 +1,37 @@
 package com.example.test
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+//import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        //enableEdgeToEdge()
 
-            // this is a ttt
+//
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+//
+//            // this is a ttt
+//        }
+        val btnSave = findViewById<Button>(R.id.btnSave)
+        btnSave.setOnClickListener{
+            val firstName = findViewById<EditText>(R.id.etFirstName)
+            val lastName = findViewById<EditText>(R.id.etLastName)
+            val statusUpdate = findViewById<TextView>(R.id.tvStatusUpdate)
+            //tvStatusUpdate.text.to
         }
+
+        //}
     }
 }
