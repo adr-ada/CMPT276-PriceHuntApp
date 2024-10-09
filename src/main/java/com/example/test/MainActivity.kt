0234153ce0302ced10320d2da719e946c153dbd8
Mylo-hz <kaunghtetzaw2514@gmail.com>
2024-10-08 19:52:53 -0700

    test3

--- src/main/java/com/example/test/MainActivity.kt
+++ src/main/java/com/example/test/MainActivity.kt
@@ -16,7 +16,7 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
 
-            // this is a test
+            // this is a test2
         }
     }
 }
