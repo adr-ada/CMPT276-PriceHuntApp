6c0ba6d8168071ed59abecd3da4ddb165bb9af45
Mylo-hz <kaunghtetzaw2514@gmail.com>
2024-10-08 19:53:26 -0700

    test12345

--- src/main/java/com/example/test/MainActivity.kt
+++ src/main/java/com/example/test/MainActivity.kt
@@ -16,7 +16,7 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
 
-            // this is a test2
+            // this is a ttt
         }
     }
 }
